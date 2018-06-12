849d01fd11bd35a6ec0f7545acf59748f4c676cf
ClassyEwok <40008061+ClassyEwok@users.noreply.github.com>
2018-06-12 15:01:14 -0500

    Update proj02_02.py

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -2,12 +2,51 @@
 # Date:
 
 # proj02_02: Fibonaci Sequence
-
+amt = int(input("How many Fibonacci numbers would you like to make? "))
+pren = 0
+curn = 1
+for number in range(0,amt):
+    print(curn)
+    nexn = pren + curn
+    pren = curn
+    curn = nexn
 """
 Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci 
 sequence is a sequence of numbers where the next number in the sequence is the sum of the 
 previous two numbers in the sequence. The sequence looks like this: 
 1, 1, 2, 3, 5, 8, 13...
 """
+
+amount = int(input("How many powers of two? "))
+two = 2
+for number in range(0,amount):
+    print (two)
+    nextone = two * 2
+    two = nextone
+
+amnt = int(input("How many Fibonacci numbers would you like to make? "))
+n = 0
+prenu = 0
+curnu = 1
+while n < amnt:
+    print(curnu)
+    nexnu = prenu + curnu
+    prenu = curnu
+    curnu = nexnu
+    n = n + 1
+
+amtn = int(input("What number for divisors? "))
+
+for divisor in range(1, amtn):
+    if amtn%divisor == 0:
+        print(str(divisor))
+
+
+
+
+
+
+
+
 
 
