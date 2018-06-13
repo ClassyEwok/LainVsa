b2db8c7ae77829e4104da333d40cb783c8deb69f
ClassyEwok <40008061+ClassyEwok@users.noreply.github.com>
2018-06-13 15:32:49 -0500

    Update proj03.py

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -1,60 +1,43 @@
 # Name:
 # Date:
+import random
 
-# proj02_02: Fibonaci Sequence
-amt = int(input("How many Fibonacci numbers would you like to make? "))
-pren = 0
-curn = 1
-for number in range(0,amt):
-    print(curn)
-    nexn = pren + curn
-    pren = curn
-    curn = nexn
+print("Guessing Game!")
+print("(type exit to end game)")
+guess = input("Guess a number between 1 and 9: ")
+random_number = random.randint(1,9)
+amntguess = 0
+while int(guess) != random_number and int(guess) != 0 or amntguess < 4:
+    if int(guess) < random_number:
+        print("That is too low!")
+        amntguess = amntguess + 1
+        guess = input("Guess a number between 1 and 9: ")
+        if guess == "exit" or amntguess == 4:
+            guess = 0
+            print("The game has been Terminated.")
+    elif int(guess) > random_number:
+        print("That is too high!")
+        amntguess = amntguess + 1
+        guess = input("Guess a number between 1 and 9: ")
+        if guess == "exit" or amntguess == 4:
+            guess = 0
+            print("The game has been Terminated.")
+    else:
+        amntguess = amntguess + 1
+        print("You got it in " + str(amntguess) + " guesses!")
+    #if  int(guess) > 9:
+        #print("No don't do that.")
+        #guess = 0
+
+
+
+
+""" 
+proj 03: Guessing Game
+
+Generate a random number between 1 and 9 (including 1 and 9). 
+Ask the user to guess the number, then tell them whether they guessed too low, too high, 
+or exactly right. Keep the game going until the user types exit.
+Keep track of how many guesses the user has taken, and when the game ends, print this out.
+
 """
-Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci 
-sequence is a sequence of numbers where the next number in the sequence is the sum of the 
-previous two numbers in the sequence. The sequence looks like this: 
-1, 1, 2, 3, 5, 8, 13...
-"""
-
-amount = int(input("How many powers of two? "))
-two = 2
-for number in range(0,amount):
-    print (two)
-    nextone = two * 2
-    two = nextone
-
-amnt = int(input("How many Fibonacci numbers would you like to make? "))
-n = 0
-prenu = 0
-curnu = 1
-while n < amnt:
-    print(curnu)
-    nexnu = prenu + curnu
-    prenu = curnu
-    curnu = nexnu
-    n = n + 1
-
-amtn = int(input("What number for divisors? "))
-
-for divisor in range(1, amtn):
-    if amtn%divisor == 0:
-        print(str(divisor))
-
-amtofnumbers = 0
-numb = float(input("Enter a number to average, put a zero when done: "))
-average = numb
-while numb != 0:
-    number = float(input("Enter a number to average, put a zero when done: "))
-    total = numb + number
-    numb = number
-    amtofnumbers = amtofnumbers + 1
-    average = total / amtofnumbers
-print("Average is " + str(average))
-
-
-
-
-
-
-
