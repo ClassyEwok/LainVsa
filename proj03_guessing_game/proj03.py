1123646234a29c0064e9e35d20670a8340d80f55
ClassyEwok <40008061+ClassyEwok@users.noreply.github.com>
2018-06-13 11:01:51 -0500

    Update proj03.py

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -1,15 +1,60 @@
 # Name:
 # Date:
 
-
-""" 
-proj 03: Guessing Game
-
-Generate a random number between 1 and 9 (including 1 and 9). 
-Ask the user to guess the number, then tell them whether they guessed too low, too high, 
-or exactly right. Keep the game going until the user types exit.
-Keep track of how many guesses the user has taken, and when the game ends, print this out.
-
+# proj02_02: Fibonaci Sequence
+amt = int(input("How many Fibonacci numbers would you like to make? "))
+pren = 0
+curn = 1
+for number in range(0,amt):
+    print(curn)
+    nexn = pren + curn
+    pren = curn
+    curn = nexn
 """
+Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci 
+sequence is a sequence of numbers where the next number in the sequence is the sum of the 
+previous two numbers in the sequence. The sequence looks like this: 
+1, 1, 2, 3, 5, 8, 13...
+"""
+
+amount = int(input("How many powers of two? "))
+two = 2
+for number in range(0,amount):
+    print (two)
+    nextone = two * 2
+    two = nextone
+
+amnt = int(input("How many Fibonacci numbers would you like to make? "))
+n = 0
+prenu = 0
+curnu = 1
+while n < amnt:
+    print(curnu)
+    nexnu = prenu + curnu
+    prenu = curnu
+    curnu = nexnu
+    n = n + 1
+
+amtn = int(input("What number for divisors? "))
+
+for divisor in range(1, amtn):
+    if amtn%divisor == 0:
+        print(str(divisor))
+
+amtofnumbers = 0
+numb = float(input("Enter a number to average, put a zero when done: "))
+average = numb
+while numb != 0:
+    number = float(input("Enter a number to average, put a zero when done: "))
+    total = numb + number
+    numb = number
+    amtofnumbers = amtofnumbers + 1
+    average = total / amtofnumbers
+print("Average is " + str(average))
+
+
+
+
+
 
 
