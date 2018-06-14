aeb9d4efc986e4f889163fef9992c9a97d1075e7
ClassyEwok <40008061+ClassyEwok@users.noreply.github.com>
2018-06-14 14:56:35 -0500

    Update proj05_hangman.py

--- proj05_hangman/proj05_hangman.py
+++ proj05_hangman/proj05_hangman.py
@@ -19,14 +19,22 @@ def load_words():
     Depending on the size of the word list, this function may
     take a while to finish.
     """
-    print "Loading word list from file..."
+    print("Loading word list from file...")
     # inFile: file
-    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    wordlist = []
+   # inFile = open(WORDLIST_FILENAME, 'r')
+    for line in open('words.txt'):
+        separator = " "
+        line = line.split(separator)
+        for value in line:
+            wordlist.append(value)
+
     # line: string
-    line = inFile.readline()
+    #line = inFile.readline()
     # wordlist: list of strings
-    wordlist = string.split(line)
-    print "  ", len(wordlist), "words loaded."
+    #wordlist = str.split(" ")
+    #print (wordlist)
+    print ("  " + str(len(wordlist)) + "words loaded.")
     return wordlist
 
 def choose_word(wordlist):
@@ -45,4 +53,57 @@ def choose_word(wordlist):
 # in the program
 wordlist = load_words()
 
+word = choose_word(wordlist)
+print(word)
+
 # your code begins here!
+amtletters = []
+wordlist = []
+amtguess = 6
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+wordansw = []
+counter = 0
+for letters in word:
+    amtletters.append(letters)
+print("Hello and welcome to Hang Man!")
+print("Don't lose. The president's life is at stake!")
+print("the word has " + str(len(amtletters)) + " letters in it.")
+for value in amtletters:
+    wordlist.append("_ ")
+wordlist_clean = "".join(wordlist)
+letterguess = input("Guess a letter: ")
+while amtguess != 0 and len(wordansw) != len(amtletters):
+    if letterguess in (alphabet):
+        alphabet.remove(letterguess)
+        clean_alphabet = "".join(alphabet)
+        print("Letters left: " + clean_alphabet)
+    wordlist_clean = "".join(wordlist)
+    if letterguess in amtletters:
+        print("You got a letter! ")
+        counter = 0
+        for letter in amtletters:
+            if letterguess == letter:
+                wordlist[counter] = letterguess
+            counter = counter + 1
+        print("It now is: " )
+        print(" ".join(wordlist))
+        letterguess = input("Guess another letter: ")
+    else:
+        print("Wrong letter!")
+        amtguess = amtguess - 1
+        print("Still just: " + wordlist_clean)
+        letterguess = input("Guess another letter: ")
+        
+        counter = 0
+        for letter in amtletters:
+            if letterguess == letter:
+                wordlist[counter] = letterguess
+            counter = counter + 1
+        print("It now is: " )
+        print(" ".join(wordlist))
+        letterguess = input("Guess another letter: ")
+if amtletters == 0:
+    print ("You failed. The president has died.")
+elif amtletters == wordlist:
+    print("You got the word!")
+#elif
